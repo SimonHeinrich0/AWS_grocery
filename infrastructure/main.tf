a9f80982cc82dadb93b48f0ebdadd91782a84799
SimonHeinrich0 <simonheinrich.b@gmail.com>
2025-05-06 16:59:50 +0200

    Add Terraform infrastructure folder with main.tf, variables.tf and outputs.tf

---
+++ infrastructure/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_security_group" "grocery_sg" {
+  name        = "grocerymate-sg"
+  description = "Allow HTTP, SSH, and port 5000"
+  vpc_id      = "vpc-07f53ab21506dcb63"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "App Port 5000"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "grocerymate-sg"
+  }
+}
+
+resource "aws_instance" "grocerymate" {
+  ami          		 = "ami-09eb4311cbaecf89d"
+  instance_type          = "t3.micro"
+  key_name 		 = "EC2_First_Instance"
+  vpc_security_group_ids = [aws_security_group.grocery_sg.id]
+
+  tags = {
+    Name = "grocerymate-ec2"
+  }
+}
